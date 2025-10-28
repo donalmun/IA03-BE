6090c7758ba690677ed0708e2bf7894724e09789
donalmun <hoanghonhatminh@gmail.com>
2025-10-28 10:03:02 +0700

    fix(backend): implement secure CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions = {
+    origin: process.env.CORS_ORIGIN || '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
