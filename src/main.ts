53284366a885bea75129018f67d9b773b690f482
donalmun <hoanghonhatminh@gmail.com>
2025-11-05 11:50:28 +0700

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,28 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const clientOrigins = process.env.CORS_ORIGIN;
+
+  if (!clientOrigins) {
+    throw new Error('CORS_ORIGIN is not defined in the environment variables.');
+  }
+
+  // Tách chuỗi thành một mảng các origin
+  const whitelist = clientOrigins.split(',');
+
   const corsOptions = {
-    origin: process.env.CORS_ORIGIN || '*',
+    origin: whitelist, // Sử dụng mảng các origin được phép
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
+
   app.enableCors(corsOptions);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Accepting requests from origins: ${whitelist.join(', ')}`);
 }
 bootstrap();
